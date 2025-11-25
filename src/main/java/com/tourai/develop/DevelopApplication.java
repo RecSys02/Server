fb2c7ad3619ae5380cd6316cccb6b57e5191c3e1
parksaegyeol <dmsgkrkt1@naver.com>
2025-11-25 21:30:20 +0900

    refactor : EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/tourai/develop/DevelopApplication.java
+++ src/main/java/com/tourai/develop/DevelopApplication.java
@@ -2,8 +2,10 @@ package com.tourai.develop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DevelopApplication {
 
 	public static void main(String[] args) {
