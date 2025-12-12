4d67194df1836bd7b089ab7fccb58131ea78ed9d
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-12 17:18:48 +0900

    feat: SecurityConfiguration 클래스 구현

--- src/main/java/com/tourai/develop/config/SecurityConfig.java
+++ src/main/java/com/tourai/develop/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.tourai.develop.config;
 
+import com.tourai.develop.jwt.JwtFilter;
 import com.tourai.develop.jwt.JwtUtil;
+import com.tourai.develop.jwt.LoginFilter;
+import com.tourai.develop.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +11,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +35,41 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+                                           AuthenticationManager authenticationManager,
+                                           CustomUserDetailsService customUserDetailsService) throws Exception {
+
+        httpSecurity
+                .csrf((auth) -> auth.disable());
+
+        httpSecurity
+                .formLogin((auth) -> auth.disable());
+
+        httpSecurity
+                .httpBasic((auth) -> auth.disable());
+
+        httpSecurity
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+
+        LoginFilter loginFilter = new LoginFilter(authenticationManager, jwtUtil);
+        loginFilter.setFilterProcessesUrl("/login");
+
+        httpSecurity
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        httpSecurity
+                .addFilterBefore(new JwtFilter(jwtUtil, customUserDetailsService), LoginFilter.class);
+
+
+        httpSecurity
+                .sessionManagement((session)
+                        -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return httpSecurity.build();
     }
 
