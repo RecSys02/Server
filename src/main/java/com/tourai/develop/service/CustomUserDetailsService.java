8387534a7bc0284e3d5eb40e40a9f4789590af9c
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-12 16:03:26 +0900

    rename: CustomUserDetailsService 로 오타 수정

--- src/main/java/com/tourai/develop/service/CustomUserDetailsService.java
+++ src/main/java/com/tourai/develop/service/CustomUserDetailsService.java
@@ -9,11 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
 
     private final UserRepository userRepository;
