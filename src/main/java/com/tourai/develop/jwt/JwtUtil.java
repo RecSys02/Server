b52eff158bd8d2dfe643d6aa9aadfa68a5f6707c
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-17 10:44:32 +0900

    feat: 쿠키 기능 추가

--- src/main/java/com/tourai/develop/jwt/JwtUtil.java
+++ src/main/java/com/tourai/develop/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.tourai.develop.jwt;
 
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -19,14 +20,19 @@ public class JwtUtil {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-    public String createJwt(String username, String role, Long expiredMs) {
+    public String createJwt(String tokenType, String username, String role, Long expiredMs) {
         return Jwts.builder()
+                .claim("tokenType", tokenType)
                 .claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public String getTokenType(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("tokenType", String.class);
     }
 
     public boolean isExpired(String token) {
@@ -45,5 +51,21 @@ public class JwtUtil {
         return Jwts.parser().verifyWith(secretKey)
                 .build().parseSignedClaims(token).getPayload()
                 .get("role", String.class);
+    }
+
+    public Cookie createCookie(String key, String value, Long expiredMs) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setPath("/");
+        cookie.setMaxAge((int) (expiredMs / 1000));
+        cookie.setHttpOnly(true);
+        return cookie;
+    }
+
+    public Cookie deleteCookie(String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+        return cookie;
     }
 }
