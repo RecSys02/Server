bc5e7665aacccdfcabd6efe41b96f9b4274d9159
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-17 10:45:01 +0900

    feat: redis 기본 CRUD 구현

---
+++ src/main/java/com/tourai/develop/jwt/RefreshTokenUtil.java
@@ -0,0 +1,40 @@
+package com.tourai.develop.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenUtil {
+
+    private final StringRedisTemplate redisTemplate;
+
+
+    private String getKeyConvention(String userKey) {
+        return "refresh:" + userKey;
+    }
+
+    public void save(String userKey, String refreshToken, Duration duration) {
+        redisTemplate.opsForValue().set(getKeyConvention(userKey), refreshToken, duration);
+    }
+
+    public String getValue(String userKey) {
+        return redisTemplate.opsForValue().get(getKeyConvention(userKey));
+    }
+
+    public void delete(String userKey) {
+        redisTemplate.delete(getKeyConvention(userKey));
+    }
+
+    public boolean isMatch(String userKey, String refreshToken) {
+        String savedRefreshToken = getValue(userKey);
+        return savedRefreshToken != null && savedRefreshToken.equals(refreshToken);
+    }
+
+
+}
