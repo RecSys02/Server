c24206ca3806ab0fde12a80b6c1febc00a9047b1
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-21 21:44:29 +0900

    refactor: filterChain에 OAuth2 관련 설정 코드 추가

--- src/main/java/com/tourai/develop/config/SecurityConfig.java
+++ src/main/java/com/tourai/develop/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.tourai.develop.config;
 
 import com.tourai.develop.jwt.*;
+import com.tourai.develop.oauth2.CustomOAuth2UserService;
+import com.tourai.develop.oauth2.CustomSuccessHandler;
 import com.tourai.develop.service.CustomUserDetailsService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,11 +26,7 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final RefreshTokenService refreshTokenService;
     private final RedisLogoutHandler redisLogoutHandler;
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -38,7 +36,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
                                            AuthenticationManager authenticationManager,
-                                           CustomUserDetailsService customUserDetailsService) throws Exception {
+                                           CustomUserDetailsService customUserDetailsService,
+                                           CustomSuccessHandler customSuccessHandler) throws Exception {
 
         httpSecurity
                 .csrf((auth) -> auth.disable());
@@ -52,12 +51,14 @@ public class SecurityConfig {
         httpSecurity
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join",
-                                "/auth/reissue").permitAll()
+                                "/auth/reissue", "/oauth2/**",
+                                "/login/oauth2/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
 
 
         LoginFilter loginFilter = new LoginFilter(authenticationManager, jwtUtil, refreshTokenService);
+        loginFilter.setPostOnly(true);
         loginFilter.setFilterProcessesUrl("/login");
 
         httpSecurity
@@ -66,6 +67,14 @@ public class SecurityConfig {
 
         httpSecurity
                 .addFilterBefore(new JwtFilter(jwtUtil, customUserDetailsService), LoginFilter.class);
+
+
+        //oauth2
+        httpSecurity
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)))
+                        .successHandler(customSuccessHandler));
 
 
         httpSecurity
