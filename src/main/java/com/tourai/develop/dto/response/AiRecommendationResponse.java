02e8f0425e0c226812216220c340a8d638bbd38e
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-23 22:13:52 +0900

    refactor: Ai 에게 응답받는 dto 수정

--- src/main/java/com/tourai/develop/dto/response/AiRecommendationResponse.java
+++ src/main/java/com/tourai/develop/dto/response/AiRecommendationResponse.java
@@ -1,26 +1,32 @@
 package com.tourai.develop.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.tourai.develop.domain.enumType.Category;
+import com.tourai.develop.domain.enumType.Province;
 
 import java.util.List;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AiRecommendationResponse(
         List<CategoryAndRecommendedItems> recommendations
 
 ) {
 
     public record CategoryAndRecommendedItems(
-       String category,
-       List<RecommendedItem>items
-    ){}
+            Category category,
+            List<RecommendedItem> items
+    ) {
+    }
 
 
     public record RecommendedItem(
-       String region,
-       String category,
-       @JsonProperty("place_id")
-       Long placeId,
-       Double score
+            Category category,
+            Province province,
+            Long placeId,
+            Double score
 
-    ){}
+    ) {
+    }
 }
