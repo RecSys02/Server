04a0432fdc13fb008422fd43570fdcb1a9e7a7ee
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-23 22:12:25 +0900

    refactor: 수정된 dto 에 맞게 RecommendationService 수정

--- src/main/java/com/tourai/develop/service/RecommendationService.java
+++ src/main/java/com/tourai/develop/service/RecommendationService.java
@@ -1,19 +1,23 @@
 package com.tourai.develop.service;
 
 import com.tourai.develop.client.FastApiClient;
+import com.tourai.develop.domain.entity.Place;
 import com.tourai.develop.domain.entity.User;
 import com.tourai.develop.domain.entity.UserTag;
+import com.tourai.develop.domain.enumType.Category;
 import com.tourai.develop.domain.enumType.TagType;
-import com.tourai.develop.dto.RecommendationDto;
+import com.tourai.develop.dto.SelectedPlaceDto;
+import com.tourai.develop.dto.request.RecommendationRequestDto;
 import com.tourai.develop.dto.request.AiRecommendationRequest;
 import com.tourai.develop.dto.response.AiRecommendationResponse;
+import com.tourai.develop.dto.response.RecommendationResponseDto;
+import com.tourai.develop.repository.PlaceRepository;
 import com.tourai.develop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,9 @@ public class RecommendationService {
 
     private final FastApiClient fastApiClient;
     private final UserRepository userRepository;
+    private final PlaceRepository placeRepository;
 
-    public AiRecommendationResponse recommend(Long userId, RecommendationDto recommendationDto) {
+    public RecommendationResponseDto recommend(Long userId, RecommendationRequestDto recommendationRequestDto) {
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 id 의 user 가 존재하지 않습니다!, userId : " + userId));
@@ -40,16 +45,27 @@ public class RecommendationService {
 
         activityLevel = insertUserTagData(preferredThemes, preferredMoods, preferredRestaurantTypes, preferredCafeTypes, avoid, activityLevel, userTags);
 
-        AiRecommendationRequest aiRecommendationRequest = new AiRecommendationRequest(userId, preferredThemes, preferredMoods, preferredRestaurantTypes, preferredCafeTypes,
-                avoid, activityLevel, recommendationDto.city(),
-                recommendationDto.companionType(),
-                recommendationDto.budget(),
-                recommendationDto.visitCafe(),
-                recommendationDto.visitRestaurant(),
-                recommendationDto.visitTourspot(),
-                recommendationDto.lastSelectedPoi());
+        List<SelectedPlaceDto> historyPlaces =
+                (recommendationRequestDto.historyPlaces() == null) ? Collections.emptyList() : recommendationRequestDto.historyPlaces();
+        List<SelectedPlaceDto> selectedPlaces =
+                (recommendationRequestDto.selectedPlaces() == null) ? Collections.emptyList() : recommendationRequestDto.selectedPlaces();
 
-        return fastApiClient.requestRecommendation(aiRecommendationRequest);
+        AiRecommendationRequest aiRecommendationRequest = new AiRecommendationRequest(
+                userId,
+                preferredThemes,
+                preferredMoods,
+                preferredRestaurantTypes,
+                preferredCafeTypes,
+                avoid,
+                activityLevel,
+                recommendationRequestDto.region(),
+                recommendationRequestDto.companion(),
+                recommendationRequestDto.budget(),
+                historyPlaces,
+                selectedPlaces
+        );
+        AiRecommendationResponse aiRecommendationResponse = fastApiClient.requestRecommendation(aiRecommendationRequest);
+        return convertToFrontResponse(aiRecommendationResponse);
     }
 
 
@@ -59,33 +75,74 @@ public class RecommendationService {
                 TagType findTagType = userTag.getTag().getTagType();
                 String findTagName = userTag.getTag().getName();
                 switch (findTagType) {
-                    case THEME:
-                        preferredThemes.add(findTagName);
-                        break;
-                    case MOOD:
-                        preferredMoods.add(findTagName);
-                        break;
+                    case THEME -> preferredThemes.add(findTagName);
 
-                    case ACTIVITY_LEVEL:
-                        activityLevel = findTagName;
-                        break;
+                    case MOOD -> preferredMoods.add(findTagName);
 
-                    case RESTAURANT:
-                        preferredRestaurantTypes.add(findTagName);
-                        break;
+                    case ACTIVITY_LEVEL -> activityLevel = findTagName;
 
-                    case CAFE:
-                        preferredCafeTypes.add(findTagName);
-                        break;
+                    case RESTAURANT -> preferredRestaurantTypes.add(findTagName);
 
-                    case AVOID:
-                        avoid.add(findTagName);
-                        break;
-                    default:
-                        break;
+                    case CAFE -> preferredCafeTypes.add(findTagName);
+
+                    case AVOID -> avoid.add(findTagName);
                 }
             }
         }
         return activityLevel;
+    }
+
+
+    private RecommendationResponseDto convertToFrontResponse(AiRecommendationResponse aiResponse) {
+
+        List<RecommendationResponseDto.RecommendedPlaceDto> tourspot = new ArrayList<>();
+        List<RecommendationResponseDto.RecommendedPlaceDto> restaurants = new ArrayList<>();
+        List<RecommendationResponseDto.RecommendedPlaceDto> cafes = new ArrayList<>();
+
+        if (aiResponse == null || aiResponse.recommendations() == null) {
+            return new RecommendationResponseDto(tourspot, restaurants, cafes);
+        }
+
+        for (AiRecommendationResponse.CategoryAndRecommendedItems recommendation : aiResponse.recommendations()) {
+            Category recommendationCategory = recommendation.category();
+            if (recommendation.items() == null) continue;
+            for (AiRecommendationResponse.RecommendedItem item : recommendation.items()) {
+                Place findPlace = placeRepository.findByPlaceIdAndCategoryAndProvince(item.placeId(), item.category(), item.province()).orElse(null);
+                if (findPlace == null) continue;
+                switch (recommendationCategory) {
+
+                    case TOURSPOT -> {
+                        RecommendationResponseDto.RecommendedPlaceDto recommendedPlaceDto = new RecommendationResponseDto.RecommendedPlaceDto(
+                                findPlace.getId(), findPlace.getPlaceId(), findPlace.getName(), findPlace.getLatitude(),
+                                findPlace.getLongitude(), findPlace.getAddress(), findPlace.getDescription(),
+                                findPlace.getDuration(), findPlace.getImages(), findPlace.getKeywords(),
+                                findPlace.getCategory(), findPlace.getProvince()
+                        );
+                        tourspot.add(recommendedPlaceDto);
+                    }
+                    case RESTAURANT -> {
+                        RecommendationResponseDto.RecommendedPlaceDto recommendedPlaceDto = new RecommendationResponseDto.RecommendedPlaceDto(
+                                findPlace.getId(), findPlace.getPlaceId(), findPlace.getName(), findPlace.getLatitude(),
+                                findPlace.getLongitude(), findPlace.getAddress(), findPlace.getDescription(),
+                                findPlace.getDuration(), findPlace.getImages(), findPlace.getKeywords(),
+                                findPlace.getCategory(), findPlace.getProvince()
+                        );
+                        restaurants.add(recommendedPlaceDto);
+                    }
+                    case CAFE -> {
+                        RecommendationResponseDto.RecommendedPlaceDto recommendedPlaceDto = new RecommendationResponseDto.RecommendedPlaceDto(
+                                findPlace.getId(), findPlace.getPlaceId(), findPlace.getName(), findPlace.getLatitude(),
+                                findPlace.getLongitude(), findPlace.getAddress(), findPlace.getDescription(),
+                                findPlace.getDuration(), findPlace.getImages(), findPlace.getKeywords(),
+                                findPlace.getCategory(), findPlace.getProvince()
+                        );
+                        cafes.add(recommendedPlaceDto);
+                    }
+
+                }
+            }
+        }
+
+        return new RecommendationResponseDto(tourspot, restaurants, cafes);
     }
 }
