d26728e831415e0f964a16db615688dfaded95a5
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-23 11:26:50 +0900

    refactor: Tag 엔티티 Category 를 TagType 로 변경

--- src/main/java/com/tourai/develop/domain/entity/Tag.java
+++ src/main/java/com/tourai/develop/domain/entity/Tag.java
@@ -1,6 +1,7 @@
 package com.tourai.develop.domain.entity;
 
 import com.tourai.develop.domain.enumType.Category;
+import com.tourai.develop.domain.enumType.TagType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,7 +19,7 @@ public class Tag {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private Category category;
+    private TagType tagType;
 
     @Column(nullable = false, length = 50)
     private String name;
---
+++ src/main/java/com/tourai/develop/domain/enumType/TagType.java
@@ -0,0 +1,10 @@
+package com.tourai.develop.domain.enumType;
+
+public enum TagType {
+    THEME,          // 테마
+    MOOD,           // 분위기
+    ACTIVITY_LEVEL, // 활동 강도
+    RESTAURANT,     // 음식
+    CAFE,           // 카페
+    AVOID,          // 피하고 싶은 곳
+}
