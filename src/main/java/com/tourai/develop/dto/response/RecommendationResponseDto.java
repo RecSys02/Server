e9e2194b5f67d14969051947d17aaa1448d00efd
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-23 21:50:00 +0900

    feat: 추천이후 프론트에게 응답하는 dto

---
+++ src/main/java/com/tourai/develop/dto/response/RecommendationResponseDto.java
@@ -0,0 +1,30 @@
+package com.tourai.develop.dto.response;
+
+import com.tourai.develop.domain.enumType.Category;
+import com.tourai.develop.domain.enumType.Province;
+import com.tourai.develop.dto.request.RecommendationRequestDto;
+
+import java.util.List;
+
+public record RecommendationResponseDto(
+        List<RecommendedPlaceDto> tourspot,
+        List<RecommendedPlaceDto> restaurants,
+        List<RecommendedPlaceDto> cafes
+) {
+    public record RecommendedPlaceDto(
+            Long id,                 // DB PK
+            Long placeId,            // 외부 place_id
+            String name,
+            Double latitude,
+            Double longitude,
+            String address,
+            String description,
+            String duration,
+            List<String> images,     // null 가능
+            List<String> keywords,   // null 가능
+            Category category,
+            Province province
+    ) {
+    }
+
+}
