6f952965f7cfd75395483b6e73c75c492d2db74e
parksaegyeol <dmsgkrkt1@naver.com>
2025-12-23 22:14:06 +0900

    refactor: Ai 에게 요청하는 dto 수정

--- src/main/java/com/tourai/develop/dto/request/AiRecommendationRequest.java
+++ src/main/java/com/tourai/develop/dto/request/AiRecommendationRequest.java
@@ -1,11 +1,11 @@
 package com.tourai.develop.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.tourai.develop.dto.PoiDto;
+import com.tourai.develop.dto.SelectedPlaceDto;
 
 import java.util.List;
-
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AiRecommendationRequest(
 
@@ -20,19 +20,16 @@ public record AiRecommendationRequest(
 
 
         //Ai 추천 최초 실행 시 입력받는 정보
-        String city,
-        List<String> companionType,
+        String region,
+        List<String> companion,
         String budget,
 
 
         //지금까지 사용자가 선택했던 장소들
-        List<PoiDto> visitCafe,
-        List<PoiDto> visitRestaurant,
-        List<PoiDto> visitTourspot,
-
+        List<SelectedPlaceDto> historyPlaces,
 
         //사용자가 마지막으로 선택한 장소들
-        List<PoiDto> lastSelectedPoi
+        List<SelectedPlaceDto> selectedPlaces
 
 
 ) {
