9a9c533ea3e87f42a85bac875eecda3d93f5a5be
parksaegyeol <dmsgkrkt1@naver.com>
2026-01-14 13:34:11 +0900

    feat: 인기 Plan 조회 API 요청 허용 추가

--- core/src/main/java/com/tourai/develop/config/SecurityConfig.java
+++ core/src/main/java/com/tourai/develop/config/SecurityConfig.java
@@ -73,6 +73,7 @@ public class SecurityConfig {
                                 "/error").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/api/plans",
+                                "/api/plans/popular",
                                 "/api/plans/*",
                                 "/api/plans/user/*",
                                 "/api/places/*",
