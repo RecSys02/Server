648ce420c084070ddcc7d743e1fb8094abbe64dc
LNemo <kkmm0512@naver.com>
2026-01-24 12:17:47 +0900

    feat: CORS 설정 및 구성 파일 추가
    - 각 서비스(api-gateway, core, chat-server)에 CORS 설정 추가
    - CorsConfig 및 CorsProperties 클래스 생성
    - application.yml 및 application-prod.yml 파일에 CORS 허용 도메인 설정 추가
    - Swagger UI 설정 및 허용 URL 수정

---
+++ chat-server/src/main/java/com/example/chatserver/properties/CorsProperties.java
@@ -0,0 +1,19 @@
+package com.example.chatserver.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private List<String> allowedOrigins = new ArrayList<>();
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
